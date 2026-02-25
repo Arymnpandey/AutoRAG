e95649f40486ef9bb7f08613cde24c9aa8bd41e7
Aryaman Pandey <137550059+Arymnpandey@users.noreply.github.com>
2026-02-26 00:24:18 +0530

    Fix session_state vectorstore bug

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ import streamlit as st
 st.set_page_config(page_title="AutoRAG", layout="wide")
 st.title("AutoRAG Chatbot")
 
+
+
+if "vectorstore" not in st.session_state:
+    st.session_state.vectorstore = None
+
 uploaded_files=st.sidebar.file_uploader(
     "Upload your documents (PDF, TXT, CSV, Excel, Word, JSON)", 
     accept_multiple_files=True
@@ -16,18 +21,23 @@ if uploaded_files:
         docs = load_uploaded_documents(uploaded_files)
 
         vectorstore = FaissVectorStore()
-        vectorstore.build_from_documents(docs)   
+        vectorstore.build_from_documents(docs)  
+        st.session_state.vectorstore = vectorstore 
         st.success("Documents processed successfully!")
 query = st.text_input("Ask a question:")
 
 if query:
-    search_engine = RAGSearch(vectorstore)
-    response = search_engine.search_and_summarize(query)
-
-    st.markdown("### 🤖 Answer")
+    if st.session_state.vectorstore is None:
+        st.warning("Please upload documents first !!")
+    else:
+        search_engine = RAGSearch(vectorstore)
+        response = search_engine.search_and_summarize(query)
+        st.markdown("### 🤖 Answer")
     
-    #st.write(type(response))
-    st.write(response)
+        #st.write(type(response))
+        st.write(response)
+    
 
+    
    
 
